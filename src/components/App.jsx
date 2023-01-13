62bae774f9b3c755c94ebfee56e79260b0f34515
Saleryv <seryoginvaleriy@gmail.com>
2023-01-13 11:26:32 +0200

    feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,57 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import  { Component }  from 'react';
+
+import { Statistics } from './Statistics/Statistics';
+import { Section } from './Section/Section';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Notification } from './Notification/Notification';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleVote = e => {
+    const { name } = e.target;
+    this.setState(prevState => ({ [name]: prevState[name] + 1 }));
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good, neutral, bad } = this.state;
+    return ((good / (good + neutral + bad)) * 100) || 0;
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const percentage = this.countPositiveFeedbackPercentage();
+    // const percentage = Number(this.countPositiveFeedbackPercentage().toFixed(0));
+    return (
+      <>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.handleVote}
+          />
+          {this.countTotalFeedback() < 1 ? (
+            <Notification message="No feedback given" />
+          ) : (
+            <Statistics
+              good = {good}
+              neutral = {neutral}
+              bad = {bad}
+              total = {total}
+              positivePercentage = {percentage}
+            />
+          )}
+        </Section>
+      </>
+    );
+  }
+}
